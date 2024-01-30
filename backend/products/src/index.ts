be5ac547a52dd924a8bea028f366487003511c8d
Uriel Ofir <urielofir@gmail.com>
2024-01-30 19:54:42 +0200

    add bodyparser backend

--- backend/products/src/index.ts
+++ backend/products/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from './api/router';
 import cors from 'cors';
 
 
 const app = express();
 const port = 3000;
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(cors());
 
