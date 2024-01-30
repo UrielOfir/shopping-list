2b6d07b7e2cb7569870be2acba39dd9af6b6b792
Uriel Ofir <urielofir@gmail.com>
2024-01-30 19:55:08 +0200

    cleaner code

--- backend/products/src/typeorm/index.ts
+++ backend/products/src/typeorm/index.ts
@@ -16,15 +16,6 @@ const AppDataSource = new DataSource({
   logging: true,
 });
 
-// to initialize the initial connection with the database, register all entities
-// and "synchronize" database schema, call "initialize()" method of a newly created database
-// once in your application bootstrap
-// AppDataSource.initialize()
-//   .then(() => {
-//     // here you can start to work with your database
-//   })
-//   .catch((error) => console.log(error));
-
 AppDataSource.initialize()
   .then(() => {
     console.log('Data Source has been initialized!');
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,34 @@
-import React, {useState} from 'react';
-import AddItemForm from './components/AddItemForm';
-import ShoppingList from './components/ShoppingList';
-import TotalItemsDisplay from './components/TotalItemsDisplay';
-import {Button, Grid} from "@mui/material";
+import React, { useState } from "react";
+import AddItemForm from "./components/AddItemForm";
+import ShoppingList from "./components/ShoppingList";
+import TotalItemsDisplay from "./components/TotalItemsDisplay";
+import { Button, Grid } from "@mui/material";
 import OrderForm from "./components/OrderForm.tsx";
 
 const App: React.FC = () => {
-    const [showItems, setShowItems] = useState(false);
+  const [showItems, setShowItems] = useState(false);
 
   return (
-    <Grid  container
-           ml={20}
-           direction="column"
-           justifyContent="center"
-           alignItems="center"
-           style={{ minHeight: '100vh' }}>
-        {showItems?<OrderForm/>:<>
-            <AddItemForm/>
-            <TotalItemsDisplay/>
-            <ShoppingList/>
-        </>}
-        <Button onClick={()=>setShowItems(!showItems)}>השלם</Button>
+    <Grid
+      container
+      ml={20}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }}
+    >
+      {showItems ? (
+        <OrderForm />
+      ) : (
+        <>
+          <AddItemForm />
+          <TotalItemsDisplay />
+          <ShoppingList />
+        </>
+      )}
+      <Button onClick={() => setShowItems(!showItems)}>השלם</Button>
     </Grid>
   );
-}
+};
 
 export default App;
