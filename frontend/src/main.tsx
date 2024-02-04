e457349c36ffafc7c1bf4de1f353dcedb46da109
Uriel Ofir <urielofir@gmail.com>
2024-02-04 12:29:20 +0200

    move to redux toolkit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createStore} from "redux";
 import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
 import {rootReducer} from "./store";
 
-const store = createStore(
-    rootReducer,
-);
+const store = configureStore({
+    reducer: rootReducer,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
